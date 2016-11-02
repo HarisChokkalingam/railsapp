5cbb54c59314e830703b1d50349f925b97b5a980
HarisChokkalingam <harischokkalingam@gmail.com>
2016-11-02 10:19:59 -0400

    Add files via upload

---
+++ Sample/app/controllers/static_pages_controller.rb
@@ -0,0 +1,20 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def about
+  end
+
+  def project
+  end
+
+  def materials
+  end
+  
+  def workers
+  end
+  
+  def contact
+  end
+  
+end
